0a488daca48c250f934deb0daaf1a3f7b7c1f8ae
danielinux7 <daniel.abzakh@gmail.com>
2015-02-20 16:16:39 -0800

    Create bitcoin.js

---
+++ bitcoin.js
@@ -0,0 +1,118 @@
+// Here is a utility function.
+	 function findANDsort(items, symbols){
+	 	var processedData = []
+	 	try{
+	 		items = JSON.parse(items);
+	 		symbols.forEach(function(symbol){
+	 			if(items[symbol]){
+	 				var item = {'symbol': items[symbol].display_name,'bid':items[symbol].rates.bid,
+	 				'ask':items[symbol].rates.ask,'high':items[symbol].rates.last, 'low':items[symbol].rates.bid};
+	 				processedData.push(item);
+	 			}
+	 		});
+	 		return processedData;
+	 	}
+	 	catch(e){
+	 		console.log('This is the inner try catch block :' + e);
+	 		return [];
+	 	};
+	 }
+// Here is the service
+	 function exchangeService(resolve, reject) {
+	 	var xhr = new XMLHttpRequest();
+	 	xhr.open('get', 'https://api.bitcoinaverage.com/exchanges/USD');
+				// xhr.onreadystatechange = handler; refactor here ..
+				xhr.send();
+				xhr.addEventListener('load',
+					function () {
+						if (this.status == 200) {
+							resolve(this.response);
+						}
+						else if (this.status == 403) {
+							reject('error: Unauthorized');
+						}
+						else if (this.status == 404) {
+							reject('error: Not Found');
+						}
+						else if (this.status == 500) {
+							reject('error: Internal Server Error');
+						}
+						else {
+							reject('error: Unknown');
+						}
+					});
+				xhr.addEventListener('error',
+					function () {
+						reject('error');
+
+					});
+			}
+// Here is the controller.
+			function exchangeController(uitems){
+
+				var pstats = new Promise(exchangeService);
+				pstats.then(function (result) {
+					// ['bitstamp','btce','bitfinex','bitex','coinmate','itbit','hitbtc','campbx'] is hardcoded for now.
+					var processedData = findANDsort(result,['bitstamp','btce','bitfinex','bitex','coinmate','itbit','hitbtc','campbx']);
+					uitems.forEach(function(uitem, index){
+						var green = 'linear-gradient(to right, #eeeeee, #009977, #eeeeee)';
+						var orange = 'linear-gradient(to right, #eeeeee, #ff9900, #eeeeee)';
+						var blue = 'linear-gradient(to right, #0099ff, #eeeeee, #0099ff)';
+						var item = processedData[index];
+						if(item != undefined){
+							(uitem.getElementsByClassName("market"))[0].innerHTML = item.symbol;
+							(uitem.getElementsByClassName("buy"))[0].innerHTML = item.bid;
+							(uitem.getElementsByClassName("sell"))[0].innerHTML = item.ask;
+							(uitem.getElementsByClassName("high"))[0].innerHTML = item.high;
+							(uitem.getElementsByClassName("low"))[0].innerHTML = item.low;
+						}
+						if(item != undefined){
+							if(cookie == null || cookie == undefined){
+									 cookie = JSON.stringify(processedData);
+							}
+							else{
+								try{
+							  cookie = eval(cookie);
+								// cookie = findANDsort(cookie, ['bitstamp','btce','bitfinex','bitex','coinmate','itbit','hitbtc','campbx']);
+								if(index < cookie.length){
+									(uitem.getElementsByClassName("buy"))[0].style.background = (cookie[index].bid < item.bid? green : (cookie[index].bid > item.bid?orange : blue));
+									setTimeout(function(){
+										(uitem.getElementsByClassName("buy"))[0].style.background = blue;
+									},500);
+									(uitem.getElementsByClassName("sell"))[0].style.background = (cookie[index].ask < item.ask? green : (cookie[index].ask > item.ask?orange : blue));
+									setTimeout(function(){
+										(uitem.getElementsByClassName("sell"))[0].style.background = blue;
+									},500);
+									(uitem.getElementsByClassName("high"))[0].style.background = (cookie[index].high < item.high? green : (cookie[index].high > item.high?orange : blue));
+									setTimeout(function(){
+										(uitem.getElementsByClassName("high"))[0].style.background = blue;
+									},500);
+									(uitem.getElementsByClassName("low"))[0].style.background = (cookie[index].low < item.low? green : (cookie[index].low > item.low?orange : blue));
+									setTimeout(function(){
+										(uitem.getElementsByClassName("low"))[0].style.background = blue;
+									},500);
+								}
+							} catch(e){ console.log('This is the outer try catch block :' + e)}
+						}
+					}
+				});
+	}, function (reason) {
+		for(var i = 0; i < data.length; i++){
+			data[i].innerHTML = reason;
+			data[i].style.background = 'linear-gradient(to right, #eeeeee, #ff9900, #eeeeee)';
+		}
+		console.log('the reason is:' + reason);
+		setTimeout(function(){
+			for(var i = 0; i < data.length; i++){
+				data[i].style.background = 'linear-gradient(to right, #0099ff, #eeeeee, #0099ff)';
+			}
+		},500);
+	});
+	}
+// Here is the program entry point.
+	HTMLCollection.prototype.forEach = Array.prototype.forEach;
+	var items = document.getElementsByClassName('item');
+	// A fake cookie for now, Chrome doesn't allow cookies for local files.
+	var cookie = null;
+	exchangeController(items);
+	setInterval(function(){exchangeController(items);},60000);
